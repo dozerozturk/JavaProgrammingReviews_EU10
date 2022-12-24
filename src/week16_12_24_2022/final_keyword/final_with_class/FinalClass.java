ee4eae997e4afc300c85c6296ecd9e654c676d81
dozerozturk <ozturkozer@yahoo.com>
2022-12-24 12:06:20 -0600

    update 12-24-22

---
+++ src/week16_12_24_2022/final_keyword/final_with_class/FinalClass.java
@@ -0,0 +1,12 @@
+package week16_12_24_2022.final_keyword.final_with_class;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class FinalClass {
+    public static void main(String[] args) {
+        // String
+        //   Arrays //is not final
+        //  Array //final
+    }
+}
